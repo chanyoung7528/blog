40a3ab165212e0f7983ddb8bbc37b5bd08801804
chanyoung7528 <chanyoung7528@gmail.com>
2025-07-03 13:39:00 +0900

    배포

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
 import PostCard from "@/components/page/posts/PostCard";
 import { Badge } from "@/components/ui/Badge";
